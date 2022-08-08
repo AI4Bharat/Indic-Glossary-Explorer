628d94971914dfa26975bf749d21d7e3e90135b4
Nishant Nayak <nishantnayak2001@gmail.com>
2022-08-08 22:29:01 +0530

    Add better sentence level searching for glossary
    Signed-off-by: Nishant Nayak <nishantnayak2001@gmail.com>

--- server/glossary/views.py
+++ server/glossary/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
@@ -6,6 +7,28 @@ from .serializers import GlossarySerializer
 
 @api_view(['POST'])
 def index(request):
+    '''
+    View to fetch glossaries for a given sentence
+    Accepted Methods: POST
+    Request Body: {
+        "input": [
+            {
+                "source": "sentence 1"
+            },
+            {
+                "source": "sentence 2"
+            }
+        ],
+        "config": {
+            "language": {
+                "sourceLanguage": "en",
+                "targetLanguage": "hi"
+            },
+            "domain": "domain",
+            "limit": 5
+        }
+    }
+    '''
     # Parse the request body
     body = request.data
 
@@ -18,14 +41,28 @@ def index(request):
     domain = body['config']['domain']
     limit = body['config']['limit']
 
-    # Filter the glossary based on the search terms
-    glossaries = Glossary.objects.filter(
-        source__in=search_input,
-        source_lang=source_lang,
-        target_lang=target_lang,
-        domain=domain
-    ).order_by('-created_at')[:limit]
+    # Define an empty list to store the glossaries
+    output = []
 
-    # Serialize and return the glossaries
-    serializer = GlossarySerializer(glossaries, many=True)
-    return Response(serializer.data)
+    # Loop through the search terms (this is pretty inefficient and could be improved with something like Elasticsearch)
+    for term in search_input:
+        # Add all the words to the queryset
+        queryset = Q()
+        for word in term.split():
+            # NOTE: Could change this to iexact if we only want the exact matches
+            queryset |= Q(source__icontains=word)
+
+        # Filter the glossary based on the search terms
+        glossaries = Glossary.objects.filter(queryset).filter(
+            source_lang=source_lang,
+            target_lang=target_lang,
+            domain=domain
+        ).order_by('-created_at')[:limit]
+
+        # Serialize the glossaries
+        serializer = GlossarySerializer(glossaries, many=True)
+        output.append(serializer.data)
+
+    return Response({
+        "output": output
+    })

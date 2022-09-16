554732221384c7f57db8f1d86e6f6d9825fa8149
abhisheksuga <82567892+abhisheksuga@users.noreply.github.com>
2022-09-16 18:33:30 +0530

    Delete server directory

--- server/glossary/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.serializers import ModelSerializer
-
-from .models import Glossary
-
-class GlossarySerializer(ModelSerializer):
-    class Meta:
-        model = Glossary
-        fields = '__all__'
